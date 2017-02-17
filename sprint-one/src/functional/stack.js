90d6071a5362e1be341f47788f753e1d002ef1c1
Ramsha Mushtaq <ramsha.a.mushtaq@gmail.com>
2017-02-17 15:48:46 -0800

    Solve stack using functional inheritance

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,27 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[size++] = value;
   };
 
   someInstance.pop = function() {
+    if(size) {
+      size--;
+    }
+
+    var popped = storage[size];
+
+    delete storage[size];
+
+    return popped;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
