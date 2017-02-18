d189904aac15d0ba735c8d3fae45d45e31165b1a
Ramsha Mushtaq <ramsha.a.mushtaq@gmail.com>
2017-02-17 16:38:04 -0800

    Implement queue using functional inheritance

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,7 @@
 var Queue = function() {
   var someInstance = {};
+  var start = 0;
+  var end = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +9,21 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[end++] = value;
   };
 
   someInstance.dequeue = function() {
+    var dequeued = storage[start];
+
+    delete storage[start];
+
+    someInstance.size() && start++;
+
+    return dequeued;
   };
 
   someInstance.size = function() {
+    return end - start;
   };
 
   return someInstance;
